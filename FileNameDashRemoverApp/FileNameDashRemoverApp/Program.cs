eb0af72d86fd7502e38744ad1a6ce368a201a6d7
madushanka71 <30933352+madushanka71@users.noreply.github.com>
2020-02-29 16:38:00 +0530

    Add files via upload

---
+++ FileNameDashRemoverApp/FileNameDashRemoverApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FileNameDashRemoverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start...");
+            RemoveDashes();
+            Console.WriteLine("End.");
+            Console.ReadKey();
+        }
+
+        public static bool RemoveDashes()
+        {
+            try
+            {
+                var currentDirectory = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                DirectoryInfo d = new DirectoryInfo(currentDirectory);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+                    File.Move(f.FullName, f.FullName.Replace("-", " ").Replace("_"," "));
+                }
+                Console.WriteLine("Renaming complete...");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error - " + ex);
+                return false;
+            }
+        }
+    }
+}
